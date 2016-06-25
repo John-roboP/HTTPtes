c086084969be3755ddae7019026f90f5cf133b92
Ryo <r.otaka@outlook.jp>
2016-06-25 18:28:28 +0900

    Initial commit

---
+++ app/src/main/java/com/t_robop/httptes/AsyncWorker.java
@@ -0,0 +1,89 @@
+package com.t_robop.httptes;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+//非同期処理
+/**
+ * Created by Ryo on 2016/06/25.
+ */
+public class AsyncWorker extends AsyncTaskLoader<String> {
+
+    private JSONObject tx0;
+ //   private String url;
+    JSONArray datase;
+    String Myname="AAAA";
+
+    public AsyncWorker(Context context) {
+       super(context);
+       // this.url = url;
+    }
+
+    @Override
+    public String loadInBackground() {
+        //非同期でインターネット接続
+
+        String json = "DDD";
+
+        HttpURLConnection connection = null;
+        try {
+
+            // URLの作成
+            URL url = new URL("http://192.168.0.31/");
+
+// 接続用HttpURLConnectionオブジェクト作成
+            connection = (HttpURLConnection) url.openConnection();
+// リクエストメソッドの設定
+            connection.setRequestMethod("GET");
+// リダイレクトを自動で許可しない設定
+            connection.setInstanceFollowRedirects(false);
+// ヘッダーの設定(複数設定可能)
+            connection.setRequestProperty("Accept-Language", "jp");
+
+// 接続
+            connection.connect();
+
+/*
+
+            try {
+
+                //url からJsonを取得する処理・・・
+
+                datase = json.getJSONArray("datas");
+
+                for (int i = 0; i < datase.length(); i++) {
+                    JSONObject data =datase.getJSONObject(i);
+
+                    // 名前を取得
+                    Myname = data.getString("name");
+                    // 年齢を取得
+                    // String age = data.getString("age");
+
+                    //   tv0.setText(Myname);
+
+
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+*/
+
+        } catch (Exception e) {
+            
+            e.printStackTrace();
+        }
+
+        return json;
+    }
+
+}
---
+++ app/src/main/java/com/t_robop/httptes/MainActivity.java
@@ -0,0 +1,75 @@
+package com.t_robop.httptes;
+
+
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+
+
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String>  {
+
+  String json="BBB";
+    TextView tv0;
+ //   JSONArray datase;
+    Bundle args = new Bundle(1);
+  //  private static final String KEY_URL_STR = "http://192.168.0.31/";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv0 = (TextView) findViewById(R.id.textView0);
+
+        getSupportLoaderManager().initLoader(0, args, this);
+
+
+
+
+    }
+
+    @Override
+    public Loader<String> onCreateLoader(int i, Bundle bundle) {
+
+        AsyncWorker loader = new AsyncWorker(getApplication());
+        loader.forceLoad(); //これでロードが始まる。AsyncTaskLoader#onStartLoading内に実装するのも可。
+        return null;
+        /**  String urlStr = args.getString(KEY_URL_STR);
+       // if (! TextUtils.isEmpty(urlStr)) {
+         return new AsyncWorker(getApplicationContext(),json);
+    //    }
+
+        **/
+
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String Myname) {
+     //   tv0.setText(tx0);
+       json=Myname;
+        json="CCC";
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> arg0) {
+        //今回は使わない
+    }
+
+    public void button00(View v){
+        tv0.setText(json);
+    }
+
+
+
+}
+
+
+
+
+
